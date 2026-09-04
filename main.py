e7037f5caec93d7a5ed41371494839341d9c10d6
Nimish Maheshwari <nimish2k23@gmail.com>
2026-09-04 16:49:44 +0530

    Data Ingested

---
+++ main.py
@@ -0,0 +1,17 @@
+from Network_Security.components.data_ingestion import DataIngestion
+from Network_Security.exception.exception import NetworkSecurityException
+from Network_Security.logging.logger import logging
+from Network_Security.entity.config_entity import DataIngestionConfig
+from Network_Security.entity.config_entity import TrainingPipelineConfig
+import sys
+
+if __name__ == "__main__":
+    try:
+        trainingpipelineconfig = TrainingPipelineConfig()
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion = DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        print(dataingestionartifact)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
