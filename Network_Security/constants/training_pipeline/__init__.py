4755fdce73c20494a5ec3e93eea725de6a7086a3
Nimish Maheshwari <nimish2k23@gmail.com>
2026-09-07 19:12:14 +0530

    Training evaluation and MLFlow

--- Network_Security/constants/training_pipeline/__init__.py
+++ Network_Security/constants/training_pipeline/__init__.py
@@ -12,6 +12,9 @@ TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "NIMISHAI"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
@@ -34,3 +37,9 @@ DATA_TRANSFORMTION_IMPUTER_PARAMS: dict = {
     "weights": "uniform"
 }
 PREPROCESSING_OBJECT_FILE_NAME: str  = "preprocessing.pkl"
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
